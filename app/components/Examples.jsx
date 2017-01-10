0d22e88db5299fee13313023f8729f5b6a979962
Charis <bob@peershipapp.com>
2017-01-10 20:08:16 +0200

    Add grid system and style examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-  <h3>Examples component</h3>
+    <div>
+  <h1 className="text-center">Examples component</h1>
+  <p>Here are a few example locations to try out</p>
+  <ol>
+    <li>
+      <Link to='/?location=Thessaloniki'>Thessaloniki, Greece</Link>
+    </li>
+    <li>
+      <Link to='/?location=Brazil'>Rio, Brazil</Link>
+    </li>
+  </ol>
+  </div>
   );
 }
 
